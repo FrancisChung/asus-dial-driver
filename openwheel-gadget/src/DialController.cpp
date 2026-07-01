55d93bc99008cfcd53fd383519c2fceffc633bd7
Francis Chung <francis.c.chung@gmail.com>
2026-07-02 00:19:19 +1000

    gadget: add DialController state machine

---
+++ openwheel-gadget/src/DialController.cpp
@@ -0,0 +1,125 @@
+#include "DialController.h"
+
+namespace {
+constexpr int kHoldThresholdMs = 400;
+}
+
+DialController::DialController(FunctionRegistry *registry, const QString &settingsPath, QObject *parent)
+    : QObject(parent), m_registry(registry),
+      m_settings(settingsPath.isEmpty() ? QSettings() : QSettings(settingsPath, QSettings::IniFormat))
+{
+    m_holdTimer.setSingleShot(true);
+    m_holdTimer.setInterval(kHoldThresholdMs);
+    connect(&m_holdTimer, &QTimer::timeout, this, &DialController::onHoldTimerFired);
+
+    const QString savedId = m_settings.value(QStringLiteral("dial/activeFunction")).toString();
+    const int savedIndex = m_registry->indexOf(savedId);
+    if (savedIndex >= 0) {
+        m_activeIndex = savedIndex;
+    } else {
+        const int volumeIndex = m_registry->indexOf(QStringLiteral("volume"));
+        m_activeIndex = volumeIndex >= 0 ? volumeIndex : 0;
+    }
+}
+
+bool DialController::isMenuOpen() const { return m_menuOpen; }
+int DialController::highlightedIndex() const { return m_highlightedIndex; }
+
+QString DialController::activeFunctionId() const
+{
+    DialFunction *function = m_registry->at(m_activeIndex);
+    return function ? function->id() : QString();
+}
+
+int DialController::functionCount() const { return m_registry->count(); }
+
+QString DialController::displayNameAt(int index) const
+{
+    DialFunction *function = m_registry->at(index);
+    return function ? function->displayName() : QString();
+}
+
+QString DialController::iconNameAt(int index) const
+{
+    DialFunction *function = m_registry->at(index);
+    return function ? function->iconName() : QString();
+}
+
+bool DialController::isAvailableAt(int index) const
+{
+    DialFunction *function = m_registry->at(index);
+    return function && function->isAvailable();
+}
+
+void DialController::onRotated(int direction)
+{
+    if (!m_enabled) {
+        return;
+    }
+
+    const int count = m_registry->count();
+    if (count == 0) {
+        return;
+    }
+
+    if (m_menuOpen) {
+        m_highlightedIndex = ((m_highlightedIndex + direction) % count + count) % count;
+        emit highlightedIndexChanged();
+        return;
+    }
+
+    DialFunction *function = m_registry->at(m_activeIndex);
+    if (function && function->isAvailable()) {
+        function->adjust(direction);
+        emit hudRequested(function->iconName(), function->currentValueLabel());
+    }
+}
+
+void DialController::onPressChanged(bool pressed)
+{
+    if (!m_enabled) {
+        return;
+    }
+
+    m_pressed = pressed;
+    if (pressed) {
+        m_holdTimer.start();
+        return;
+    }
+
+    if (m_holdTimer.isActive()) {
+        m_holdTimer.stop();
+        return; // quick click released before hold threshold: no v1 action defined
+    }
+
+    if (m_menuOpen) {
+        m_activeIndex = m_highlightedIndex;
+        m_menuOpen = false;
+        DialFunction *function = m_registry->at(m_activeIndex);
+        if (function) {
+            m_settings.setValue(QStringLiteral("dial/activeFunction"), function->id());
+        }
+        emit menuOpenChanged();
+        emit activeFunctionChanged();
+    }
+}
+
+void DialController::setEnabled(bool enabled)
+{
+    m_enabled = enabled;
+    if (!m_enabled) {
+        m_holdTimer.stop();
+        if (m_menuOpen) {
+            m_menuOpen = false;
+            emit menuOpenChanged();
+        }
+    }
+}
+
+void DialController::onHoldTimerFired()
+{
+    m_menuOpen = true;
+    m_highlightedIndex = m_activeIndex;
+    emit menuOpenChanged();
+    emit highlightedIndexChanged();
+}
---
+++ openwheel-gadget/tests/test_dialcontroller.cpp
@@ -0,0 +1,118 @@
+#include <QtTest/QtTest>
+#include <QSignalSpy>
+#include <QTemporaryFile>
+#include "DialController.h"
+#include "FunctionRegistry.h"
+#include "FakeDialFunction.h"
+
+class TestDialController : public QObject {
+    Q_OBJECT
+private slots:
+    void quickRotateAdjustsActiveFunctionWithoutOpeningMenu();
+    void holdingPastThresholdOpensMenu();
+    void rotatingWhileMenuOpenMovesHighlight();
+    void releasingWhileMenuOpenConfirmsSelection();
+    void disablingClosesMenuAndIgnoresInput();
+
+private:
+    QString tempSettingsPath();
+};
+
+QString TestDialController::tempSettingsPath()
+{
+    static QTemporaryFile file;
+    file.setAutoRemove(true);
+    file.open();
+    return file.fileName();
+}
+
+void TestDialController::quickRotateAdjustsActiveFunctionWithoutOpeningMenu()
+{
+    FunctionRegistry registry;
+    FakeDialFunction volume("volume");
+    registry.registerFunction(&volume);
+    DialController controller(&registry, tempSettingsPath());
+
+    controller.onRotated(1);
+
+    QCOMPARE(volume.adjustCallCount, 1);
+    QCOMPARE(volume.lastDirection, 1);
+    QVERIFY(!controller.isMenuOpen());
+}
+
+void TestDialController::holdingPastThresholdOpensMenu()
+{
+    FunctionRegistry registry;
+    FakeDialFunction volume("volume");
+    registry.registerFunction(&volume);
+    DialController controller(&registry, tempSettingsPath());
+    QSignalSpy spy(&controller, &DialController::menuOpenChanged);
+
+    controller.onPressChanged(true);
+    QVERIFY(spy.wait(1000));
+    QVERIFY(controller.isMenuOpen());
+}
+
+void TestDialController::rotatingWhileMenuOpenMovesHighlight()
+{
+    FunctionRegistry registry;
+    FakeDialFunction volume("volume");
+    FakeDialFunction scroll("scroll");
+    registry.registerFunction(&volume);
+    registry.registerFunction(&scroll);
+    DialController controller(&registry, tempSettingsPath());
+
+    controller.onPressChanged(true);
+    QSignalSpy spy(&controller, &DialController::menuOpenChanged);
+    QVERIFY(spy.wait(1000));
+
+    controller.onRotated(1);
+    QCOMPARE(controller.highlightedIndex(), 1);
+    QCOMPARE(volume.adjustCallCount, 0);
+    QCOMPARE(scroll.adjustCallCount, 0);
+}
+
+void TestDialController::releasingWhileMenuOpenConfirmsSelection()
+{
+    FunctionRegistry registry;
+    FakeDialFunction volume("volume");
+    FakeDialFunction scroll("scroll");
+    registry.registerFunction(&volume);
+    registry.registerFunction(&scroll);
+    DialController controller(&registry, tempSettingsPath());
+
+    controller.onPressChanged(true);
+    QSignalSpy menuSpy(&controller, &DialController::menuOpenChanged);
+    QVERIFY(menuSpy.wait(1000));
+
+    controller.onRotated(1);
+    controller.onPressChanged(false);
+
+    QVERIFY(!controller.isMenuOpen());
+    QCOMPARE(controller.activeFunctionId(), QStringLiteral("scroll"));
+
+    controller.onRotated(1);
+    QCOMPARE(scroll.adjustCallCount, 1);
+    QCOMPARE(volume.adjustCallCount, 0);
+}
+
+void TestDialController::disablingClosesMenuAndIgnoresInput()
+{
+    FunctionRegistry registry;
+    FakeDialFunction volume("volume");
+    registry.registerFunction(&volume);
+    DialController controller(&registry, tempSettingsPath());
+
+    controller.onPressChanged(true);
+    QSignalSpy menuSpy(&controller, &DialController::menuOpenChanged);
+    QVERIFY(menuSpy.wait(1000));
+
+    controller.setEnabled(false);
+    QVERIFY(!controller.isMenuOpen());
+
+    controller.onRotated(1);
+    QCOMPARE(volume.adjustCallCount, 0);
+}
+
+QTEST_MAIN(TestDialController)
+#include "test_dialcontroller.moc"
